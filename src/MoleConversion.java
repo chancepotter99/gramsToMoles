9aa878e15157d5a382bd85b534eabb2bcbdecb77
chancepotter99 <chancepotter99@gmail.com>
2015-11-03 17:46:01 -0800

    Initial Commit
    This is a program that takes an input of grams of Carbon and outputs the
    measurement in moles.

---
+++ src/MoleConversion.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class MoleConversion {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner (System.in);
+		int i = 1;
+		do {
+			converter(console);
+			System.out.println("Would you like to quit? (y/n)");
+			if (console.next().equalsIgnoreCase("y")) {
+				i--;
+			} else {
+				i = 1;
+			}
+		} while (i == 1);
+		System.out.println("Thank you! Have a nice day!");
+
+	}
+	
+	public static void converter(Scanner console) {
+		System.out.println("What element is being converted?");
+		String element = console.next();
+		if (element.equalsIgnoreCase("C") || element.equalsIgnoreCase("Carbon")) {
+			carbon(console);
+			
+		}
+	}
+	
+	public static void carbon(Scanner console) {
+		System.out.println("Grams: ");
+		double grams = console.nextDouble();
+		double mole = grams / 12.01;
+		System.out.println(grams + " g of Carbon is " + mole + " moles.");
+	}
+
+}
