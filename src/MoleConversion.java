508574f8d24396bf7f9fd29c27ebc7c405c77a04
chancepotter99 <chancepotter99@gmail.com>
2015-11-03 17:53:26 -0800

    Comment on Code

--- src/MoleConversion.java
+++ src/MoleConversion.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class MoleConversion {
 
 	public static void main(String[] args) {
-		Scanner console = new Scanner (System.in);
+		Scanner console = new Scanner (System.in); // initialize Scanner
 		int i = 1;
 		do {
 			converter(console);
