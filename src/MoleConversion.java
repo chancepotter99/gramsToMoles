3bce823d8d238a57b1af2e8f1dd41b4fa14f5909
chancepotter99 <chancepotter99@gmail.com>
2015-11-17 15:53:08 -0800

    Created Separate Method for Element if Statements

--- src/MoleConversion.java
+++ src/MoleConversion.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class MoleConversion {
 
 	public static void main(String[] args) {
-		Scanner console = new Scanner (System.in); // initialize Scanner
+		Scanner console = new Scanner(System.in); // initialize Scanner
 		int i = 1;
 		do {
 			converter(console);
@@ -17,23 +17,27 @@ public class MoleConversion {
 		System.out.println("Thank you! Have a nice day!");
 
 	}
-	
+
 	public static void converter(Scanner console) {
 		System.out.println("What element is being converted?");
-		String element = console.next();
-		if (element.equalsIgnoreCase("C") || element.equalsIgnoreCase("Carbon")) {
-			carbon(console);
-			
+		String input = console.next();
+		element(input, console);
+
+	}
+
+	public static void element(String input, Scanner console) {
+		if (input.equalsIgnoreCase("C") || input.equalsIgnoreCase("Carbon")) {
+			output(input, console);
+
 		} else {
-			System.out.println("I don't know the element: " + element);
+			System.out.println("I don't know the element: " + input);
 		}
 	}
-	
-	public static void carbon(Scanner console) {
+
+	public static void output(String input, Scanner console) {
 		System.out.println("Grams: ");
 		double grams = console.nextDouble();
 		double mole = grams / 12.01; // change molar mass to a variable
 		System.out.format(grams + " g of Carbon is %.4f moles.%n", mole);
 	}
-
 }
