7a79e0aa2d33cac91db0cd66efe36b40a0cce8b6
chancepotter99 <chancepotter99@gmail.com>
2015-11-17 16:00:51 -0800

    Added Hydrogen, Oxygen, and Nitrogen

--- src/MoleConversion.java
+++ src/MoleConversion.java
@@ -26,18 +26,29 @@ public class MoleConversion {
 	}
 
 	public static void element(String input, Scanner console) {
-		if (input.equalsIgnoreCase("C") || input.equalsIgnoreCase("Carbon")) {
-			output(input, console);
-
+		double molarMass;
+		if (input.equalsIgnoreCase("H") || input.equalsIgnoreCase("Hydrogen")) {
+			molarMass = 1.008;
+			output(input, console, molarMass);
+		} else if (input.equalsIgnoreCase("N") || input.equalsIgnoreCase("Nitrogen")) {
+			molarMass = 14.01;
+			output(input, console, molarMass);
+		} else if (input.equalsIgnoreCase("O") || input.equalsIgnoreCase("Oxygen")) {
+			molarMass = 16.00;
+			output(input, console, molarMass);
+		} else if (input.equalsIgnoreCase("C") || input.equalsIgnoreCase("Carbon")) {
+			molarMass = 12.01;
+			output(input, console, molarMass);
 		} else {
 			System.out.println("I don't know the element: " + input);
 		}
+		
 	}
 
-	public static void output(String input, Scanner console) {
+	public static void output(String input, Scanner console, double molarMass) {
 		System.out.println("Grams: ");
 		double grams = console.nextDouble();
-		double mole = grams / 12.01; // change molar mass to a variable
-		System.out.format(grams + " g of Carbon is %.4f moles.%n", mole);
+		double mole = grams / molarMass; // change molar mass to a variable
+		System.out.format(grams + " g of " + input + " is %.4f moles.%n", mole);
 	}
 }
