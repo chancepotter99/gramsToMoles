ed8be4c3fab5dc3597691e1a974b6a14c960a49c
chancepotter99 <chancepotter99@gmail.com>
2015-11-18 23:02:45 -0800

    Added First 3 Periods of Periodic Table to ArrayList

--- src/MoleConversion.java
+++ src/MoleConversion.java
@@ -30,13 +30,28 @@ public class MoleConversion {
 		boolean found = false;
 		List<Element> elements = new ArrayList<Element>();
 		elements.add(new Element("H", "Hydrogen", 1.008));
+		elements.add(new Element("He", "Helium", 4.003));
+		elements.add(new Element("Li", "Lithium", 6.941));
+		elements.add(new Element("Be", "Beryllium", 9.012));
+		elements.add(new Element("B", "Boron", 10.81));
+		elements.add(new Element("C", "Carbon", 12.01));
 		elements.add(new Element("N", "Nitrogen", 14.01));
 		elements.add(new Element("O", "Oxygen", 16.00));
-		elements.add(new Element("C", "Carbon", 12.01));
+		elements.add(new Element("Fl", "Flourine", 19.00));
+		elements.add(new Element("Ne", "Neon", 20.18));
+		elements.add(new Element("Na", "Sodium", 22.99));
+		elements.add(new Element("Mg", "Magnesium", 24.31));
+		elements.add(new Element("Al", "Aluminum", 26.98));
+		elements.add(new Element("Si", "Silicon", 28.09));
+		elements.add(new Element("P", "Phosphorus", 30.97));
+		elements.add(new Element("S", "Sulfur", 32.07));
+		elements.add(new Element("Cl", "Chlorine", 35.45));
+		elements.add(new Element("Ar", "Argon", 39.95));
 		for (Element element : elements) {
 			if (element.getSymbol().equalsIgnoreCase(input) || element.getName().equalsIgnoreCase(input)) {
 				molarMass = element.getMolarMass();
-				output(input, console, molarMass);
+				String name = element.getName();
+				output(console, molarMass, name);
 				found = true;
 			}
 		}
@@ -46,11 +61,11 @@ public class MoleConversion {
 
 	}
 
-	public static void output(String input, Scanner console, double molarMass) {
+	public static void output(Scanner console, double molarMass, String name) {
 		System.out.println("Grams: ");
 		double grams = console.nextDouble();
 		double mole = grams / molarMass; // change molar mass to a variable
-		System.out.format(grams + " g of " + input + " is %.4f moles.%n", mole);
+		System.out.format(grams + " g of " + name + " is %.4f moles.%n", mole);
 	}
 
 }
