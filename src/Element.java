12068d0dfd079d1f7526b0e90f924346e8ece6d4
chancepotter99 <chancepotter99@gmail.com>
2015-12-15 17:33:03 -0800

    Element Class for attributes associated with elements.

---
+++ src/Element.java
@@ -0,0 +1,29 @@
+
+public class Element {
+	private String symbol;
+	private String name;
+	private double molarMass;
+	public Element(String symbol, String name, double molarMass) {
+		this.setSymbol(symbol);
+		this.setName(name);
+		this.setMolarMass(molarMass);
+	}
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getMolarMass() {
+		return molarMass;
+	}
+	public void setMolarMass(double molarMass) {
+		this.molarMass = molarMass;
+	}
+}
